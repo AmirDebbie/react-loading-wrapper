d229ba9d8127da0e17d0ab9cd99269ad1240dd7f
AmirDebbie <67858073+AmirDebbie@users.noreply.github.com>
2020-11-11 09:57:17 +0200

    Updates

---
+++ precache-manifest.c84ad6ce3666ab215ad80ca554ec2364.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74697449ba9b7eeb8b0d2e74fc933203",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3d688b7625f7da149b7c",
+    "url": "./static/css/main.0dec11a3.chunk.css"
+  },
+  {
+    "revision": "872628fd2750075b3f9c",
+    "url": "./static/js/2.cbd276a0.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.cbd276a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d688b7625f7da149b7c",
+    "url": "./static/js/main.9bfd7433.chunk.js"
+  },
+  {
+    "revision": "75586f083ae6ecec6fd2",
+    "url": "./static/js/runtime-main.b0102374.js"
+  }
+]);
